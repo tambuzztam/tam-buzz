4fb5eb366bd3934e7dfe78d99a116076cd872a3c
Ben Polansky <polanskyb@gmail.com>
2024-04-08 16:45:47 -0500

    fix message and seo title (#12)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,1 @@
 // Put your custom JS code here
-document.addEventListener("DOMContentLoaded", function () {
-  const divs = document.querySelectorAll(".message");
-  const randomIndex = Math.floor(Math.random() * divs.length);
-  divs[randomIndex].classList.remove("hidden");
-});
