92e8e657778c943eb4e7e5a2942f087538287e6e
Ben Polansky <polanskyb@gmail.com>
2026-02-26 13:23:03 -0600

    SEO optimization: sitemap, OG images, structured data (#30)
    * Add SEO: sitemap, robots.txt, OG images, canonical URLs, structured data
    
    * Add separate web manifest for Tam Listen

--- tam-buzz-astro/astro.config.mjs
+++ tam-buzz-astro/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://tam.buzz",
+  integrations: [tailwind(), sitemap()],
 });
